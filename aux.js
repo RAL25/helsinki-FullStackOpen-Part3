77948475ba067f8e46eba74dd19dd8cf0ecb0672
Rian Alves <rianalvesleal@gmail.com>
2026-02-11 17:39:43 -0300

    Testing the environment file

--- aux.js
+++
@@ -1,4 +0,0 @@
-console.log(process.argv.length);
-for (let i = 0; i < process.argv.length; i++) {
-  console.log(process.argv[i]);
-}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 // 3.1
 const express = require("express");
 const morgan = require("morgan");
